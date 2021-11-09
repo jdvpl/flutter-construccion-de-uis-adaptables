40899317640956eb9b5766ff4e8b788a1fe62373
jdvpl <juanda554242@gmail.com>
2021-11-09 10:55:18 -0500

    creando circulos gradientes y posicionamientos negativos

--- api_rest_ui/lib/pages/home_page.dart
+++ api_rest_ui/lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:api_rest_ui/widgets/circle.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,6 +11,38 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.white,
+        child: Stack(
+          children: [
+            Positioned(
+              top: -100,
+              right: -50,
+              child: Circle(
+                size: 340,
+                colors: [
+                  Colors.pinkAccent,
+                  Colors.pink,
+                ],
+              ),
+            ),
+            Positioned(
+              left: -50,
+              top: -100,
+              child: Circle(
+                size: 240,
+                colors: [
+                  Colors.orange,
+                  Colors.deepOrangeAccent,
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
