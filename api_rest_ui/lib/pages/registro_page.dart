51eb048dc90572562d02c9603f8dc0cae3abaca7
jdvpl <juanda554242@gmail.com>
2021-11-09 17:59:17 -0500

    comenzando con el registro

---
+++ api_rest_ui/lib/pages/registro_page.dart
@@ -0,0 +1,143 @@
+import 'dart:ui';
+
+import 'package:api_rest_ui/utils/responsive.dart';
+import 'package:api_rest_ui/widgets/input_text.dart';
+import 'package:flutter/material.dart';
+
+class RegisterForm extends StatefulWidget {
+  const RegisterForm({Key key}) : super(key: key);
+
+  @override
+  _RegisterFormState createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<RegisterForm> {
+  GlobalKey<FormState> _formKey = GlobalKey();
+  String _email = "", _password = "";
+
+  _submit() {
+    bool isOk = _formKey.currentState.validate();
+    print("formulario $isOk");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Responsive responsive = Responsive.of(context);
+    return Positioned(
+      bottom: 30,
+      child: Container(
+        constraints: BoxConstraints(
+          maxWidth: responsive.isTablet ? 4300 : 360,
+        ),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              InputText(
+                keyboardType: TextInputType.emailAddress,
+                label: "CORREO ELECTRONICO",
+                fontSize: responsive.dp(responsive.isTablet ? 1.2 : 1.5),
+                onChanged: (text) {
+                  _email = text;
+                },
+                validator: (text) {
+                  if (!text.contains("@")) {
+                    return "Correo invalido";
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(
+                height: responsive.dp(2),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      color: Colors.black26,
+                    ),
+                  ),
+                ),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: InputText(
+                        label: "CONTRASEÑA",
+                        obsucreText: true,
+                        borderEnabled: false,
+                        onChanged: (text) {
+                          _password = text;
+                        },
+                        validator: (text) {
+                          if (text.trim().length == 0) {
+                            return "Correo invalidontraseña invalidad";
+                          }
+                          return null;
+                        },
+                        fontSize:
+                            responsive.dp(responsive.isTablet ? 1.2 : 1.5),
+                      ),
+                    ),
+                    FlatButton(
+                      padding: EdgeInsets.symmetric(vertical: 10),
+                      onPressed: () {},
+                      child: Text(
+                        "Olvido Contraseña",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: responsive.dp(1.5)),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: responsive.dp(5),
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: FlatButton(
+                  padding: EdgeInsets.symmetric(vertical: 15),
+                  color: Colors.pinkAccent,
+                  onPressed: this._submit,
+                  child: Text(
+                    "Iniciar Sesion",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: responsive.dp(2),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: responsive.dp(2),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "¿No tienes cuenta?",
+                    style: TextStyle(fontSize: responsive.dp(1.5)),
+                  ),
+                  FlatButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Registrarme",
+                      style: TextStyle(
+                        color: Colors.pinkAccent,
+                        fontSize: responsive.dp(1.5),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: responsive.dp(14),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
